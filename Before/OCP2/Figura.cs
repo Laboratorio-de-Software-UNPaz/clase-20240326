f37d27dad8d65d85fc82cd1987b91cdf00c63ef6
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2024-03-26 08:49:39 -0300

    Initial commit

---
+++ Before/OCP2/Figura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP2
+{
+    public class Figura
+    {
+        private string tipo;
+
+        public Figura(string tipo)
+        {
+            this.tipo = tipo;
+        }
+
+        public double area()
+        {
+            if (tipo == "cuadrado")
+            {
+                // Devuelve cálculo área del cuadrado
+                return 1;
+            } else if (tipo == "rectangulo")
+            {
+                // Devuelve cálculo área del rectangulo
+                return 2;
+            } else
+            {
+                // Devuelve cálculo área del circulo
+                return 3;
+            }
+        }
+    }
+}
---
+++ Before/OCP2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OCP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura cuadrado = new Figura("cuadrado");
+            Console.WriteLine(cuadrado.area());
+
+            Figura rectangulo = new Figura("rectangulo");
+            Console.WriteLine(rectangulo.area());
+
+            Figura circulo = new Figura("circulo");
+            Console.WriteLine(circulo.area());
+        }
+    }
+}
